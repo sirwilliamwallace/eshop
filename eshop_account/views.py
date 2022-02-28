885f19339b056cc7c02f03accaf25eb91a38dacb
numberx <shekooha696@gmail.com>
2022-02-28 15:42:35 +0330

    renders templates of login and register in eshop_accounts app

--- eshop_account/views.py
+++ eshop_account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login(request):
+    context = {}
+    return render(request, 'authentication/login.html', context)
+
+
+def register(request):
+    context = {}
+    return render(request, 'authentication/register.html', context)
