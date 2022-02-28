866ba30e35e88774002705a72a455f38eba405f5
numberx <shekooha696@gmail.com>
2022-02-28 13:51:02 +0330

    made function based views for footer and header

--- Eshop/views.py
+++ Eshop/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
 
+def header(request, *args, **kwargs):
+    context = {
+        "title": "It's a Eshops"
+    }
+    return render(request, 'shared/Header.html', context)
+
+
+def footer(request, *args, **kwargs):
+    context = {
+        "about_us": "I'm nUmberX",
+    }
+    return render(request, 'shared/Footer.html', context)
+
+
 def home_page(request):
     context = {
         'data': 'Hello World'
