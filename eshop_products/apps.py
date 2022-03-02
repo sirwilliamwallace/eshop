287788d7624a8b6e500e4f12b60c446f71f52f59
numberx <shekooha696@gmail.com>
2022-03-02 13:06:06 +0330

    added persion verbose name for model

--- eshop_products/apps.py
+++ eshop_products/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class EshopProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'eshop_products'
+    verbose_name = 'ماژول محصولات'
