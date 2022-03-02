c84537470d35b6b28ab8df91a70b833d93d6f735
numberx <shekooha696@gmail.com>
2022-03-02 16:00:37 +0330

    added products detail view

--- eshop_products/views.py
+++ eshop_products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.views.generic import ListView, DetailView
 from .models import Product
 
@@ -17,3 +18,18 @@ class ProductListView(ListView):
             case, active products)
         """
         return Product.objects.get_active_products()
+
+
+def product_detail_view(request, *args, **kwargs):
+    """
+    View for displaying a single product.
+    TODO: make class based view for this view
+    """
+    product_id = kwargs.get('id')
+    product = Product.objects.get_by_id(id=product_id)
+    if product is None or not product.active:
+        raise Http404("محصول مورد نظر یافت نشد")
+    context = {
+        'product': product
+    }
+    return render(request, 'products/product_detail.html', context)
