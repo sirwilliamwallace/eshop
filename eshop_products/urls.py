6db7d8a2aaf420469646bd3026d6ea1d710869ca
numberx <shekooha696@gmail.com>
2022-03-02 16:00:17 +0330

    added url for detail view

--- eshop_products/urls.py
+++ eshop_products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProductListView
+from .views import ProductListView, product_detail_view
 
 app_name = 'eshop_products'
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/<id>/<title>', product_detail_view, name='product_detail'),
 ]
