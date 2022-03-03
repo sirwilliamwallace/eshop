c7c1444fc1ccabe36a23d17fa96fd99e085b8f1a
numberx <shekooha696@gmail.com>
2022-03-03 12:16:04 +0330

    url for search view /products/search

--- eshop_products/urls.py
+++ eshop_products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProductListView, product_detail_view
+from .views import ProductListView, product_detail_view, SearchProducts
 
 app_name = 'eshop_products'
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product_list'),
     path('products/<id>/<title>', product_detail_view, name='product_detail'),
+    path('products/search',SearchProducts.as_view() , name='search_products'),
 ]
