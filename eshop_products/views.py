310e3731f4305832ba596105bb2e920727fd6611
numberx <shekooha696@gmail.com>
2022-03-05 14:03:55 +0330

    a list for containg images list that contains the images

--- eshop_products/views.py
+++ eshop_products/views.py
@@ -1,8 +1,11 @@
+import itertools
+
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product , Gallery
 from eshop_products_categories.models import ProductsCategories
+
 
 class ProductListView(ListView):
     """
@@ -19,25 +22,33 @@ class ProductListView(ListView):
         """
         return Product.objects.get_active_products()
 
+
 class ProductCategoriesListView(ListView):
     """
     View for listing all products in the database.
     This view is used to display products in a category.
     """
-    template_name = 'products/product_list.html' # Default template name
+    template_name = 'products/product_list.html'  # Default template name
     paginate_by = 10  # Show "n" products per page
 
-    def get_queryset(self): # Override the get_queryset method
+    def get_queryset(self):  # Override the get_queryset method
         """
          get queryset is a method of ListView class that returns a list of objects to be displayed on the page (in this
             case, active products)
         """
-        category_name = self.kwargs['category'] # Get the category name from the URL
-        category = ProductsCategories.objects.filter(slug__iexact=category_name).filter() # Get the category object
+        category_name = self.kwargs['category']  # Get the category name from the URL
+        category = ProductsCategories.objects.filter(slug__iexact=category_name).filter()  # Get the category object
         # print(category)
-        if category is None: # If the category does not exist
-            raise Http404("صفحه مورد نظر یافت نشد") # Raise an error
-        return Product.objects.get_product_by_categories(categorie_name=category_name) # Return the products in the category
+        if category is None:  # If the category does not exist
+            raise Http404("صفحه مورد نظر یافت نشد")  # Raise an error
+        return Product.objects.get_product_by_categories(
+            categorie_name=category_name)  # Return the products in the category
+
+
+def gallery_iterate(n, iterable):
+    args = [iter(iterable)] * n
+    return ([e for e in t if e is not None] for t in itertools.zip_longest(*args))
+
 
 def product_detail_view(request, *args, **kwargs):
     """
@@ -48,8 +59,12 @@ def product_detail_view(request, *args, **kwargs):
     product = Product.objects.get_by_id(id=product_id)
     if product is None or not product.active:
         raise Http404("محصول مورد نظر یافت نشد")
+
+    gallery = Gallery.objects.filter(product_id=product_id)
+    iterated_galleries = list(gallery_iterate(3, gallery))
     context = {
-        'product': product
+        'product': product,
+        'galleries': iterated_galleries
     }
     return render(request, 'products/product_detail.html', context)
 
