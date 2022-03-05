305c3ca388dcac79b3ec21c58869caff337da38b
numberx <shekooha696@gmail.com>
2022-03-05 11:22:51 +0330

    url and name for categorie partial view!

--- eshop_products/urls.py
+++ eshop_products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import ProductListView, product_detail_view, SearchProducts
+from .views import ProductListView, product_detail_view, SearchProducts, ProductCategoriesListView, category_partial_view
 
 app_name = 'eshop_products'
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product_list'),
     path('products/<id>/<title>', product_detail_view, name='product_detail'),
-    path('products/search',SearchProducts.as_view() , name='search_products'),
+    path('products/search', SearchProducts.as_view(), name='search_products'),
+    path('products/<category>/', ProductCategoriesListView.as_view(), name='category_products'),
+    path('categories_partial_view', category_partial_view, name='category_partial_view'),
 ]
