51e7437330f1b1e41b197bfb084818595e23c7f9
numberx <shekooha696@gmail.com>
2022-03-07 12:49:40 +0330

    urls include

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('',include("eshop_account.urls")),
     path('', include("eshop_products.urls")),
     path('', include("eshop_contact.urls")),
+    path('', include("eshop_orders.urls")),
     path('header', header, name='header'),
     path('footer', footer, name='footer'),
     path('admin/', admin.site.urls),
