c42ac1796716401a27046bc29c5f1ea194fed122
numberx <shekooha696@gmail.com>
2022-03-07 12:48:41 +0330

    Implemented Order and OrderDetail for handling shopping card in site

---
+++ eshop_orders/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'eshop_orders.apps.EshopOrdersConfig'
---
+++ eshop_orders/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+from eshop_products.models import Product
+
+
+class Order(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    isPaid = models.BooleanField(default=False, verbose_name='پرداخت شده')
+    paymentDate = models.DateTimeField(null=True, blank=True, verbose_name='تاریخ پرداخت')
+
+    def __str__(self):
+        return self.owner.get_full_name()
+
+    class Meta:
+        verbose_name = 'سفارش'
+        verbose_name_plural = 'سفارشات'
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='سفارش')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='محصول')
+    gheymat = models.IntegerField(verbose_name='قیمت')
+    tedad = models.IntegerField(verbose_name='تعداد')
+
+    def __str__(self):
+        return self.product.title
+    class Meta:
+        verbose_name = 'جزئیات سفارش'
+        verbose_name_plural = 'جزئیات سفارش'
