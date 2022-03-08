791ba17f8e69c2fabf424a6f057246489c0b8477
numberx <shekooha696@gmail.com>
2022-03-08 09:56:26 +0330

    fixed some issues with url

--- eshop_contact/urls.py
+++ eshop_contact/urls.py
@@ -4,5 +4,5 @@ from .views import contact_us
 
 app_name = 'eshop_contact'
 urlpatterns = [
-    path('/contact-us', contact_us, name='contact'),
+    path('contact-us', contact_us, name='contact'),
 ]
