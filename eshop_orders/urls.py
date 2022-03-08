d5878ded75278b930cd563b40fd07a46aad5753e
numberx <shekooha696@gmail.com>
2022-03-08 09:57:43 +0330

    url for card detail

--- eshop_orders/urls.py
+++ eshop_orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add_order
+from .views import add_order, card_detail
 
 app_name = 'eshop_orders'
 urlpatterns = [
     path('add-order', add_order, name='add_order'),
+    path('order-detail', card_detail, name='order_detail'),
 ]
